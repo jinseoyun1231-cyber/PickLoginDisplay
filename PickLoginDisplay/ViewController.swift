697858a8af2c23f01a8b54f42720bd609b802e63
Sally <jin.seoyun1231@gmail.com>
2026-05-12 17:26:26 +0900

    Initial Commit

---
+++ PickLoginDisplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PickLoginDisplay
+//
+//  Created by Seoyun Jin on 5/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
